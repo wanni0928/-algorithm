e2fda363938f44c2fe3c3e7327c403c6d16cceac
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-06-27 23:17:38 +0900

    linked list

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -47,6 +47,38 @@ node = head
 for index in range(3, 11):
     add(index)
 
+# while node.next:
+#     print(node.data)
+#     node = node.next
+# print (node.data)
+
+'''
+링크드 리스트의 장단점 (전통적인 C언어에서의 배열과 링크드 리스트)
+    - 장점
+        1. 미리 데이터 공간을 할당하지 않아도 됨. : 배열은 미리 데이터 공간을 할당해야 한다.
+    - 단점
+        1. 연결을 위한 별도 데이터 공간이 필요하므로, 저장공간 효율이 높지 않다.
+        2. 연결 정보를 찾는 시간이 필요하므로 접근 속도가 느림
+        3. 중간 데이터 삭제시, 앞뒤 데이터의 견결을 재구성해야 하는 부가적인 작업 필요    
+'''
+
+## 링크드 리스트의 복잡한 기능 1(링크드 리스트 데이터 사이에 데이터를 추가)
+    ## 링크드 리스트는 유지 관리에 부가적인 구현이 필요함.
+
+node3 = Node(1.5)
+node = head
+search = True
+
+while search:
+    if node.data == 1:
+        search = False
+    else:
+        node = node.next
+
+node_next = node.next
+node.next = node3
+node3.next = node_next
+
 while node.next:
     print(node.data)
     node = node.next
