b77821508012956c2791a99248bcde553285b972
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-06-27 23:39:57 +0900

    linked list

--- linked_list/linked_list2.py
+++ linked_list/linked_list2.py
@@ -21,6 +21,28 @@ class NodeMgmt:
         while node:
             print(node.data)
             node = node.next
+    
+    # 1. head 삭제
+    # 2. 마지막 노드 삭제
+    # 3. 중간 노드 삭제
+
+    def delete(self, data):
+        if self.head == '':
+            print('no data to delete')
+            return
+        if self.head.data == data:
+            ## delete head
+            temp = self.head
+            self.head = self.head.next
+            del temp
+        else:
+            node = self.head
+            while node.next:
+                if node.next.data == data:
+                    temp = node.next
+                    node.next = node.next.next
+                    del temp
+
 
 linkedList = NodeMgmt(0)
 
