f6a3e9db33f2b571389dcace1c68e4446acfa0a9
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-07-03 11:08:20 +0900

    chanining

--- hash_table/hash_table_03.py
+++ hash_table/hash_table_03.py
@@ -1,3 +1,15 @@
+## 충돌(Collision) 해결 알고리즘(좋은 해쉬 함수 사용하기)
+'''
+Chaining 기법
+    - 개방 해슁 또는 Open Hashing 기법 중 하나:해쉬 테이블 저장공간 외의 공간을 활용하는 기법
+    - 충돌이 일어나면, 링크드 리스트라는 자료 구조를 사용해서, 링크드 리스트로 데이터를 추가로 연결시켜서 저장하는 기법.
+'''
+
+'''
+연습: 테이블 코드에 Chaining 기법으로 충돌 해결 코드를 추가해보기
+1. 해쉬 함수 : key % 8
+2. 해쉬 키 생성 : hash(data)
+'''
 hash_table = list([0 for i in range(8)])
 
 def get_key(data):
@@ -9,6 +21,7 @@ def hash_function(key):
 def save_data(data, value):
     index_key = get_key(data)
     hash_address = hash_function(index_key)
+    # hash_table의 디폴트 값은 0
     if hash_table[hash_address] != 0:
         for index in range(len(hash_table[hash_address])):
             if hash_table[hash_address][index][0] == index_key:
@@ -29,3 +42,13 @@ def read_data(data):
         return None
     else:
         return None
+
+print(hash('Dava') % 8)
+print(hash('Data') % 8)
+print(hash('Dd') % 8)
+
+save_data('Dd', '1201023010')
+save_data('Data', '3301023010')
+print(read_data('Dd'))
+
+print(hash_table)
