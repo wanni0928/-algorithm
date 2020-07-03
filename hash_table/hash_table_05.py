017a957ff709e9c930f06919e018db75bad6fd72
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-07-03 11:54:44 +0900

    Linear Probing

--- hash_table/hash_table_05.py
+++ hash_table/hash_table_05.py
@@ -1,24 +1,21 @@
-'''
-Linear Probing 기법
-    - 폐쇄 해슁 또는 Closing Hashing 기법 중 하나: 해쉬 테이블 저장공간 안에서 충돌 문제를 해결하는 기법
-    - 충돌이 일어나면, 해당 hash address의 다음 address부터 맨 처음 나오는 빈 공간에 저장하는 기법
-        : 저장공간 활용도를 높이기 위한 기법.
-'''
+import hashlib
 
 hash_table = list([0 for i in range(8)])
 
 def get_key(data):
-    return hash(data)
+        hash_object = hashlib.sha256()
+        hash_object.update(data.encode())
+        hex_dig = hash_object.hexdigest()
+        return int(hex_dig, 16)
 
 def hash_function(key):
     return key % 8
 
 def save_data(data, value):
     index_key = get_key(data)
-    hsah_address = hash_function(index_key)
-
-    if hash_table[hsah_address] != 0:
-        for index in range(hsah_address, len(hash_table)):
+    hash_address = hash_function(index_key)
+    if hash_table[hash_address] != 0:
+        for index in range(hash_address, len(hash_table)):
             if hash_table[index] == 0:
                 hash_table[index] = [index_key, value]
                 return
@@ -26,7 +23,7 @@ def save_data(data, value):
                 hash_table[index][1] = value
                 return
     else:
-        hash_table[hsah_address] = [index_key, value]
+        hash_table[hash_address] = [index_key, value]
 
 def read_data(data):
     index_key = get_key(data)
@@ -41,13 +38,10 @@ def read_data(data):
     else:
         return None
 
-print (hash('dk') % 8)
-print (hash('da') % 8)
-print (hash('dc') % 8)
+save_data('da', '01200123123')
+save_data('dh', '3333333333')
+read_data('dh')
 
-save_data('dk', '01200123123')
-save_data('da', '3333333333')
-
-print(read_data('dk'))
-print(read_data('da'))
-print(read_data('dc'))
+print (get_key('db') % 8)
+print (get_key('da') % 8)
+print (get_key('dh') % 8)
