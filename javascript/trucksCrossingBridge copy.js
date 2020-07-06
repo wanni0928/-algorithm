dc642398c9851cb76e65adcefa4608885d80bbf0
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-07-07 02:15:09 +0900

    Create trucksCrossingBridge copy.js

---
+++ javascript/trucksCrossingBridge copy.js
@@ -0,0 +1,28 @@
+function solution(bridge_length, weight, truck_weights) {
+    let times = [];
+    let on_bridge = [];
+    let seconds = 0;
+    let finish_times = 0;
+    let current_total_weight = 0;
+    
+    while(truck_weights.length > 0){
+        // console.log(truck_weights);
+        if(current_total_weight + truck_weights[0] <= weight){
+            times.push(++seconds);
+            on_bridge.push(truck_weights.shift());
+            current_total_weight += on_bridge[on_bridge.length - 1];
+        }else{
+            times.push(++seconds);
+        }
+        
+        if(seconds - times[times.length - 1] === bridge_length){
+            current_total_weight -= on_bridge[on_bridge.length - 1];
+            if(on_bridge > 0){on_bridge.pop()};
+            times.push(++seconds);
+        }
+
+    }
+
+    finish_times = bridge_length + times[times.length - 1];
+    return finish_times;
+}
