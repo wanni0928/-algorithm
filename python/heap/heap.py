07854b822b1bad19b32c4f33d84f58aa75acfa6b
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-07-09 15:47:01 +0900

    Create heap.py

---
+++ python/heap/heap.py
@@ -0,0 +1,40 @@
+class Heap:
+    def __init__(self, data):
+        self.heap_array = list()
+        self.heap_array.append(None)
+        self.heap_array.append(data)
+
+    def move_up(self, inserted_idx):
+        if inserted_idx <= 1:
+            return False
+        
+        parent_idx = inserted_idx // 2
+        print("inserted_idx", inserted_idx)
+        print("parent_idx", parent_idx)
+        if self.heap_array[inserted_idx] > self.heap_array[parent_idx]:
+            return True
+        else:
+            return False
+
+    def insert(self, data):
+        if len(self.heap_array) == 0:
+            self.heap_array.append(None)
+            self.heap_array.append(data)
+            return True
+
+        self.heap_array.append(data)
+        
+        inserted_idx = len(self.heap_array) - 1
+        while self.move_up(inserted_idx):
+            parent_idx = inserted_idx // 2
+            self.heap_array[inserted_idx], self.heap_array[parent_idx] = self.heap_array[parent_idx], self.heap_array[inserted_idx]
+            inserted_idx = parent_idx
+        return True
+
+heap = Heap(15)
+heap.insert(10)
+heap.insert(8)
+heap.insert(5)
+heap.insert(4)
+
+print(heap.heap_array)
