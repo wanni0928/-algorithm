60c02022267513653b325ef3e22e2569d4a27fb4
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-07-20 10:42:57 +0900

    Create recursive_call4.py

---
+++ python/recursive_call/recursive_call4.py
@@ -0,0 +1,11 @@
+
+import random
+data = random.sample(range(100), 10)
+print(data)
+
+def sum_list(data):
+    if len(data) <= 1:
+        return data[0]
+    return data[0] + sum_list(data[1:])
+
+print(sum_list(data))
