96a618aeaad5f0b76bc3cac3dad39024bc3b10a0
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-10-18 21:27:32 +0900

    Create result.sql

---
+++ SQL/HackerRank/type of triangle/mySQL/result.sql
@@ -0,0 +1,7 @@
+select case
+when A = B and B = C then 'Equilateral'
+when A + B <= C or A + C <= B or B + C <= A then 'Not A Triangle'
+when A = B or B = C or A = C then 'Isosceles'
+else 'Scalene'
+end as tri_type
+from triangles;
